92df3dc35c3e1a2915d68fabe44c8f563f4b49c3
dita_810 <dita_810@DESKTOP-5FIKS4P>
2025-05-29 15:35:40 +0900

    login

--- SpringTP/src/main/java/com/dita/controller/LoginController.java
+++ SpringTP/src/main/java/com/dita/controller/LoginController.java
@@ -1,5 +1,13 @@
 package com.dita.controller;
 
-public class LoginController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login"; // templates/login.html
+    }
 }
+
