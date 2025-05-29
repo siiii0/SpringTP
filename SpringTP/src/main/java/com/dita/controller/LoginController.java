7f90365d37e471cce887155a36f6a9ec2bd9ef82
skrjsdl03 <skrjsdl02@naver.com>
2025-05-29 15:54:30 +0900

    로그인

--- SpringTP/src/main/java/com/dita/controller/LoginController.java
+++ SpringTP/src/main/java/com/dita/controller/LoginController.java
@@ -5,9 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
-    @GetMapping("/login")
-    public String loginPage() {
-        return "login"; // templates/login.html
+	
+    @GetMapping(value = "/login")
+    public void loginPage() {
     }
+    
 }
 
