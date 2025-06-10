7ad3dacf89d7f694f15bd2f152ebd7d68e40e16d
dita_810 <dita_810@DESKTOP-5FIKS4P>
2025-06-10 20:51:56 +0900

    사용자 출제 문제 폼 완료

--- SpringTP/src/main/java/com/dita/controller/CodingtestController.java
+++ SpringTP/src/main/java/com/dita/controller/CodingtestController.java
@@ -72,6 +72,12 @@ public class CodingtestController {
         return "codingtest/user_Q";
     }
     
+ // 사용자 출제 문제 작성 폼 페이지
+    @GetMapping("/create_Q")
+    public String showCreateUserQuestionForm(Model model) {
+        return "codingtest/create_Q";
+    }
+    
     // 문제 풀이 페이지
     @GetMapping("/solve_Q")
     public String solveQuestion(@RequestParam("id") int id, Model model) {
